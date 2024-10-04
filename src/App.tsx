b1ff5faa914f332157f7c3c343e968246e0841ba
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-10-04 20:53:39 -0300

    Main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import Router from "~/router";
-import { Header } from "./components/Header";
+import { Header } from '~/components/Header';
+
 
 function App() {
   return (
     <>
-      <Header>
-        <h1>Caju Front Teste</h1>
-      </Header>
+      <Header/>
       <Router />
     </>
   );
