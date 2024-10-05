d0151c8cb5eb55b453ac90352b11e129b2e5ad75
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-10-05 17:47:49 -0300

    add loader component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,14 +2,16 @@ import { Collumns } from './components/Columns'
 import * as S from './styles'
 import { SearchBar } from './components/Searchbar'
 import { useRegistrations } from '~/context/registrations-context'
+import { Loading } from '~/components/Loading'
 
 const DashboardPage = () => {
-  const { registrations } = useRegistrations()
+  const { registrations, isLoading } = useRegistrations()
 
   return (
     <S.Container>
       <SearchBar />
       <Collumns registrations={registrations} />
+      {isLoading && <Loading />}
     </S.Container>
   )
 }
