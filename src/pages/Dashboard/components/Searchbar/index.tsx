320b9494fb6fc01e8a39eed6eb34950eb57c5b68
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-10-05 17:52:41 -0300

    organize SearchBar

--- src/pages/Dashboard/components/Searchbar/index.tsx
+++ src/pages/Dashboard/components/Searchbar/index.tsx
@@ -5,21 +5,28 @@ import { IconButton } from '~/components/Buttons/IconButton'
 import TextField from '~/components/TextField'
 import routes from '~/router/routes'
 import * as S from './styles'
-export const SearchBar = () => {
-  const history = useHistory()
+import { useRegistrations } from '~/context/registrations-context'
 
-  const goToNewAdmissionPage = () => {
+export function SearchBar() {
+  const history = useHistory()
+  const { refreshList } = useRegistrations()
+
+  function goToNewAdmissionPage() {
     history.push(routes.newUser)
+  }
+
+  function handleRefetch() {
+    refreshList()
   }
 
   return (
     <S.Container>
       <TextField placeholder="Digite um CPF válido" />
       <S.Actions>
-        <IconButton aria-label="refetch">
+        <IconButton onClick={handleRefetch} aria-label="refetch">
           <HiRefresh />
         </IconButton>
-        <Button onClick={() => goToNewAdmissionPage()}>Nova Admissão</Button>
+        <Button onClick={goToNewAdmissionPage}>Nova Admissão</Button>
       </S.Actions>
     </S.Container>
   )
