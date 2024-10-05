a1741e54c76fffcfc933110ecc28670963aca256
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-10-04 21:01:54 -0300

    feat: get registrations

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,24 @@
 import Collumns from "./components/Columns";
 import * as S from "./styles";
 import { SearchBar } from "./components/Searchbar";
+import { useEffect, useState } from 'react';
+import { api } from '~/api';
 
 const DashboardPage = () => {
+  const [registrations, setRegistrations] = useState([])
+  async function fetchInitialData() {
+    const response = await api.get('/registrations')
+    setRegistrations(response.data)
+  }
+
+  useEffect(() => {
+    fetchInitialData()
+  }, [])
+
   return (
     <S.Container>
       <SearchBar />
-      <Collumns registrations={[]} />
+      <Collumns registrations={registrations} />
     </S.Container>
   );
 };
