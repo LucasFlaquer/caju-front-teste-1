ab3fbd296eab5122ce9603ebac418a55cc786afa
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-10-06 17:04:11 -0300

    chore: improve DRY

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -1,32 +1,24 @@
-import { Bounce, toast } from 'react-toastify'
+import { Bounce, toast, ToastOptions } from 'react-toastify'
+
+const TOAST_OPTIONS: ToastOptions = {
+  position: 'top-right',
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: 'light',
+  transition: Bounce,
+}
 
 export function useToast() {
   function notifySuccess(message: string) {
-    toast.success(message, {
-      position: 'top-right',
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: 'light',
-      transition: Bounce,
-    })
+    toast.success(message, TOAST_OPTIONS)
   }
 
   function notifyError(message: string) {
-    toast.error(message, {
-      position: 'top-right',
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: 1,
-      theme: 'light',
-      transition: Bounce,
-    })
+    toast.error(message, TOAST_OPTIONS)
   }
 
   return { notifySuccess, notifyError }
