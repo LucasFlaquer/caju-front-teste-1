3d51b823c8e3340d6dff949e9616baefdc7e6237
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-10-08 23:39:10 -0300

    remove unused component

--- src/pages/Dashboard/components/Searchbar/components/SearchByTherm.tsx
+++ src/pages/Dashboard/components/Searchbar/components/SearchByTherm.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { MaskedInput } from '~/components/Form/TextField/MaskedInput'
-import { CPF_MASK } from '~/components/Form/validationMasks'
+import { CPF_MASK } from '~/utils/validationMasks'
 import { useRegistrations } from '~/context/registrations-context'
 import { useDebounce } from '~/hooks/useDebounce'
 import { isValidCPF } from '~/utils/validateCPF'
